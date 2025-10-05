cc9851ab303f0d423b1c8ab1f64a5cdb98b5bcde
amazingawwal <sakeebmawwal@gmail.com>
2025-10-05 23:26:56 +0100

    feat:JWT implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,6 +9,9 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
-  imports: [PassengerModule, PassportModule, JwtModule],
+  imports: [PassengerModule, PassportModule, JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_SIGN_EXP },
+    }),],
 })
 export class AuthModule {}
