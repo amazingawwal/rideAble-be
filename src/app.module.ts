6fe00334eecb4f21329940e3b6d5e0869fd3fee8
amazingawwal <sakeebmawwal@gmail.com>
2025-10-13 23:38:22 +0100

    chore:created resource - driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PassengerModule } from './passenger/passenger.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RidesModule } from './rides/rides.module';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
-  imports: [AdminModule, PassengerModule, UsersModule, AuthModule, RidesModule],
+  imports: [AdminModule, PassengerModule, UsersModule, AuthModule, RidesModule, DriverModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
